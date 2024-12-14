427b337bed35ebc5cc2a4f8bd74b9854fe2dbc08
EmJeyDev <emjeydev@gmail.com>
2024-12-14 12:39:13 +0330

    added the project

---
+++ src/main/java/dev/emjey/document_save_task/DocumentSaveTaskApplication.java
@@ -0,0 +1,13 @@
+package dev.emjey.document_save_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentSaveTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentSaveTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/emjey/document_save_task/DocumentSaveTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.emjey.document_save_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentSaveTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
